ce38a0a94de34cab7858fdbbbcb2ffe7878b0cb6
Devonjedi <devon.jedi@gmail.com>
2022-06-16 13:01:01 -0500

    test commit

---
+++ web/sites/default/files/php/twig/629a2f4637ae0_audio-tag.html.twig_HNPP0IyPFKZt88IdbNhrEQWct/roePPhGRRqg5mpBnkLYo3IrVSS7oihjQp4MVqqwGXLI.php
@@ -0,0 +1,108 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/custom/html5_audio/templates/audio-tag.html.twig */
+class __TwigTemplate_edaecb5fe20fa7889a43f62f4acd79de6a2e8ea33d8ce7778dab55429bd16d5e extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<audio controls preload=\"metadata\">
+  ";
+        // line 2
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["sources"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["source"]) {
+            // line 3
+            echo "    <source src=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["source"], "src", [], "any", false, false, true, 3), 3, $this->source), "html", null, true);
+            echo "\" type=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["source"], "mimetype", [], "any", false, false, true, 3), 3, $this->source), "html", null, true);
+            echo "\" />
+  ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['source'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 5
+        echo "
+</audio>";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/custom/html5_audio/templates/audio-tag.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  57 => 5,  46 => 3,  42 => 2,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/custom/html5_audio/templates/audio-tag.html.twig", "/var/www/html/web/modules/custom/html5_audio/templates/audio-tag.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("for" => 2);
+        static $filters = array("escape" => 3);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
